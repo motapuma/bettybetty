5f40f4d32ce3cafcffd6cd67e1a541181d0d17be
Alberto Mota <mota_al@hotmail.com>
2014-11-20 20:21:01 -0600

    bla

--- db/migrate/20141121021259_create_links.rb
+++ db/migrate/20141121021259_create_links.rb
@@ -2,7 +2,7 @@ class CreateLinks < ActiveRecord::Migration
   def change
     create_table :links do |t|
       t.string :title
-      t.content :text
+      t.text :content
       t.integer :journal
       t.string :author
       t.string :url
