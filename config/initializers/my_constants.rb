8050e374415aac5e70f431d2085cd2f8b7779555
Alberto Mota <mota_al@hotmail.com>
2014-11-22 17:04:48 -0600

    bla

--- config/initializers/my_constants.rb
+++ config/initializers/my_constants.rb
@@ -1,2 +1,2 @@
 URL_UNIVERSAL = "http://www.eluniversal.com.mx/noticias.html"
-JOURNALS = [["El Norte",URL_UNIVERSAL],["Milenio",URL_UNIVERSAL],["El provenir",URL_UNIVERSAL],["ABC",URL_UNIVERSAL],["Reporte Indigo",URL_UNIVERSAL],["Reforma","El Universal",URL_UNIVERSAL],["Excelsior",URL_UNIVERSAL],["La Jornada",URL_UNIVERSAL],["Notimex",URL_UNIVERSAL],["Agencia Efe",URL_UNIVERSAL]]
+JOURNALS = [["El Norte",URL_UNIVERSAL],["Milenio",URL_UNIVERSAL],["El provenir",URL_UNIVERSAL],["ABC",URL_UNIVERSAL],["Reporte Indigo",URL_UNIVERSAL],["Reforma","El Universal",URL_UNIVERSAL],["Excelsior",URL_UNIVERSAL],["La Jornada",URL_UNIVERSAL],["Notimex",URL_UNIVERSAL],["Agencia Efe",URL_UNIVERSAL],["Otro",URL_UNIVERSAL]]
